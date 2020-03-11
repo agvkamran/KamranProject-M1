fb9f4e8bc2e73649c34cb43b781641c24c88db82
Aghayev Kamran <aghayev189@gmail.com>
2020-03-11 15:17:03 +0400

    changes after fixes

--- js.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('button').addEventListener('click', function () {
-    let x = prompt("Введите число?");
-    let isPositive =  (x > 0);
-    console.log(isPositive);
-});
-
-console.log("b"+"a"+-"a"+"a");
